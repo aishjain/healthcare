f0e219dd3f4a543b75c07f970db8ca81cf64141e
Aishwarya Jain <aishwaryajain143@gmail.com>
2018-07-19 08:52:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# healthcare
+# server
+
+
+In Pubsub, There is a Client, and there is a Server.
+
+* Client can only send or receive from Server
+* but Server can receive from anyone, reply to that perticular mesasage, or emit to all Clients.
+
+MQTTLens, works as a Client,
+Our Client Node server(index.js) works as a Client.
+
+Client on Client cant be posible.
+Server => Node Red
---
+++ health_simulation.js
@@ -0,0 +1,89 @@
+require('dotenv').config();
+var mqtt = require('mqtt');
+
+
+// CONFIG VARIABLES
+
+ CLOUD_ADDRESS =process.env.ADDS
+//const CLIENT_ID ='mqttjs01'
+
+var client = mqtt.connect("mqtt://" + CLOUD_ADDRESS,{
+	username:process.env.USER1,
+    password:process.env.PASS,
+	keepalive: 120,
+    reconnectPeriod: 2000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+})
+
+
+
+const TIME_INTERVAL=2000;
+
+//handle incoming messages
+client.on('message', function (topic, message, packet) {
+	console.log("message is " + message);
+	console.log("topic is " + topic);
+});
+
+
+client.on("connect", function () {
+	console.log("connected  " + client.connected);
+
+})
+//handle errors
+client.on("error", function (error) {
+	console.log("Can't connect" + error);
+	process.exit(1)
+});
+
+
+//publish 
+function publish(topic, msg, options) {
+	msg=Math.round(30 + Math.random() * 50) + "";
+	console.log("publishing", msg);
+
+	if (!!client.connected) client.publish(topic, msg, options);
+
+
+}
+
+// function to publish heartbeat 
+function publishHeartbeat(value) {
+	if (!!client.connected) client.publish("heartbeat", value, options);
+
+}
+
+
+// function to publish temperature
+function publishTemperature(value) {
+	if (!!client.connected) client.publish("temperature", value, options);
+
+}
+
+//////////////
+
+var options = {
+	retain: true,
+	qos: 1
+};
+
+console.log("subscribing to topics");
+
+
+// var topic_list = ["topic2", "topic3", "topic4"];
+// var topic_o = { "topic22": 0, "topic33": 1, "topic44": 1 };
+// client.subscribe(topic, { qos: 1 }); //single topic
+// client.subscribe(topic_list, { qos: 1 }); //topic list
+// client.subscribe(topic_o); //object
+
+
+var heartbeatTimer = setInterval(function () { 
+	const value=Math.round(60 + Math.random() * 20) + "";
+	publishHeartbeat(value); }, TIME_INTERVAL);
+var temperatureTimer = setInterval(function () {
+	const value=Math.round(30 + Math.random() * 10) + "";
+	publishTemperature(value); }, TIME_INTERVAL);
+
